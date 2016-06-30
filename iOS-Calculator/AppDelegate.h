1735e2d545a967abd7ac5a2ee4a3a7f506217275
almodad <almodad@almodads-mac.local>
2016-06-30 01:23:54 -0700

    Initial Commit

---
+++ iOS-Calculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iOS-Calculator
+//
+//  Created by almodad on 6/30/16.
+//  Copyright © 2016 almodad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iOS-Calculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOS-Calculator
+//
+//  Created by almodad on 6/30/16.
+//  Copyright © 2016 almodad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
